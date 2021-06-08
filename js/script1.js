7016cd9a97512bf5979e91116e3c451e7de4ae58
RiccardoSalvalajo <salvalajo.riccardo.cartiera@gmail.com>
2021-06-08 15:37:50 +0200

    if del while pollInserito non funziona

--- js/script1.js
+++ js/script1.js
@@ -1,7 +1,7 @@
 
 var arrayCampo = []
 
-while (arrayCampo.length < (100 - 16)) {
+while (arrayCampo.length < 16) {
     var numR = ramdomNum(1,100)
     if (!arrayCampo.includes(numR)) {
         arrayCampo.push(numR)
@@ -10,12 +10,22 @@ while (arrayCampo.length < (100 - 16)) {
 
 console.log(arrayCampo);
 
+// var inserito = prompt('inserisci un numero da 1 a 100');
+var poolInserito = [];
 
+var p = 0;
 
+while (poolInserito.length < 5) {
+    var inserito = prompt('inserisci un numero da 1 a 100');
+    if (isNaN(inserito) && arrayCampo.includes(inserito))  {
+        inserito = prompt("Attenzione! inserisci un numero da 1 a 100 che non hai già inserito");
+    } else {
+        poolInserito.push(inserito);
+    }
+    p++
+}
 
-
-
-
+console.log(poolInserito);
 
 
 

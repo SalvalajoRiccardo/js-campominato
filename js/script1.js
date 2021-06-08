9e9a8139e0fc7265495f596ca8c93fbf93ccd02d
RiccardoSalvalajo <salvalajo.riccardo.cartiera@gmail.com>
2021-06-08 16:58:27 +0200

    bloccato

--- js/script1.js
+++ js/script1.js
@@ -1,31 +1,47 @@
 
 var arrayCampo = []
 
-while (arrayCampo.length < 16) {
-    var numR = ramdomNum(1,100)
+while (arrayCampo.length < 7) {
+    var numR = ramdomNum(1,20)
     if (!arrayCampo.includes(numR)) {
         arrayCampo.push(numR)
     }
 }
 
-console.log(arrayCampo);
+console.log("campo " + arrayCampo);
 
 // var inserito = prompt('inserisci un numero da 1 a 100');
 var poolInserito = [];
 
 var p = 0;
+var risultato = true;
 
-while (poolInserito.length < 5) {
-    var inserito = prompt('inserisci un numero da 1 a 100');
-    if (isNaN(inserito) && arrayCampo.includes(inserito))  {
-        inserito = prompt("Attenzione! inserisci un numero da 1 a 100 che non hai già inserito");
-    } else {
-        poolInserito.push(inserito);
+while (poolInserito.length < 5 && !arrayCampo.includes(inserito)) {
+    var inserito = (prompt('inserisci un numero da 1 a 100'));
+    while (isNaN(inserito) || poolInserito.includes(inserito))  {
+        inserito = (prompt("Attenzione! inserisci un numero da 1 a 100 che non hai già inserito"));
+    
     }
+    
     p++
 }
 
-console.log(poolInserito);
+if (arrayCampo.includes(inserito)) {
+    console.log("hai perso");
+
+} else if (!arrayCampo.includes(inserito)) {
+    poolInserito.push(inserito);
+    console.log(poolInserito);
+
+} else if(poolInserito.length == 5) {
+    console.log("Hai Vinto");
+}
+
+
+
+
+
+
 
 
 

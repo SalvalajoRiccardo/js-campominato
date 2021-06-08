f8f4c78e82995b5ac5af38b9535c79859931893b
RiccardoSalvalajo <salvalajo.riccardo.cartiera@gmail.com>
2021-06-08 17:37:45 +0200

    fine

--- js/script1.js
+++ js/script1.js
@@ -1,40 +1,40 @@
 
 var arrayCampo = []
 
-while (arrayCampo.length < 7) {
-    var numR = ramdomNum(1,20)
+while (arrayCampo.length < 16) {
+    var numR = ramdomNum(1,100)
     if (!arrayCampo.includes(numR)) {
         arrayCampo.push(numR)
     }
 }
 
-console.log("campo " + arrayCampo);
+console.log(arrayCampo);
 
 // var inserito = prompt('inserisci un numero da 1 a 100');
 var poolInserito = [];
 
 var p = 0;
-var risultato = true;
-
-while (poolInserito.length < 5 && !arrayCampo.includes(inserito)) {
-    var inserito = (prompt('inserisci un numero da 1 a 100'));
+var semaforo =true
+while (poolInserito.length < 84 && semaforo == true) {
+    var inserito = parseInt((prompt('inserisci un numero da 1 a 100')));
     while (isNaN(inserito) || poolInserito.includes(inserito))  {
-        inserito = (prompt("Attenzione! inserisci un numero da 1 a 100 che non hai già inserito"));
+        inserito = parseInt((prompt("Attenzione! inserisci un numero da 1 a 100 che non hai già inserito")));
     
     }
     
+    if (arrayCampo.includes(inserito)) {
+        console.log("hai perso");
+        semaforo = false
+    } else {
+        poolInserito.push(inserito);
+        console.log(poolInserito);
+    
+    } 
     p++
 }
 
-if (arrayCampo.includes(inserito)) {
-    console.log("hai perso");
-
-} else if (!arrayCampo.includes(inserito)) {
-    poolInserito.push(inserito);
-    console.log(poolInserito);
-
-} else if(poolInserito.length == 5) {
-    console.log("Hai Vinto");
+if (poolInserito.length == 84) {
+    console.log("hai vinto");
 }
 
 
